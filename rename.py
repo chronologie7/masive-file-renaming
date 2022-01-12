479d4bbd18ee14f2665f3987cec03a82c3e60f2c
Alfonso Farías <77316261+chronologie7@users.noreply.github.com>
2022-01-12 03:29:49 -0300

    changed for run in every system

--- rename.py
+++ rename.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+
 import os, re
 
-regex = r"(file)(_)(\d+)(\.txt)"
+regex = r"(file)(_)(\d+)(\.\w+)"
 fileList = os.walk(".", topdown=False)
 
 for root, dirs, files in fileList:
     for file in files:
         try:
             re_result = re.findall(regex, file)
-            source = root + "\\" + file
-            newName = root + "\\" + re_result[0][0] + re_result[0][2] + re_result[0][3]
-            print(f"renaming: \"{root}\\{file}...")
+            source = os.path.join(root, file)
+            newName = re_result[0][0] + re_result[0][2] + re_result[0][3]
+            newName = os.path.join(root, newName)
+            print(f"renaming: {source}...")
             os.rename(source, newName)
         except:
             continue
